c2625a95e5095e551c71c571184df54367fd9dfd
Sixpaths837 <yerasanisreeram@gmail.com>
2022-03-20 23:13:23 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -15,7 +15,6 @@ def editDistance(str1, str2):
     ----------
     str1: string
         string to be compared
-
     str2: string
         string being compared with
     Returns
@@ -24,18 +23,22 @@ def editDistance(str1, str2):
         the edit distance between the two strings
     """
     # Converting str1 to str2
+    #Initiating the matrix with correct dimensions along with the first row and column
     mat = np.zeros((len(str1) + 1, len(str2) + 1))
     for i in range(0, len(str1) + 1):
         mat[i][0] = i
     for i in range(0, len(str2) + 1):
         mat[0][i] = i
+    # Comparing the three previous terms and checking minimum of the
     for i in range(1, len(str1) + 1):
         for j in range(1, len(str2) + 1):
             if str1[i - 1] == str2[j - 1]:
                 mat[i][j] = min(mat[i - 1][j] + 1, mat[i][j - 1] + 1, mat[i - 1][j - 1])
             else:
                 mat[i][j] = min(mat[i - 1][j] + 1, mat[i][j - 1] + 1, mat[i - 1][j - 1] + 1)
+    # Returning edit distance
     return mat[len(str1)][len(str2)]
+    # While implementing function keep a track of len(str1)-len(str2) so we can reduce usage of editDistance()
 
 
 def findRightWord(word):
@@ -58,3 +61,4 @@ def booleanRet(query):
     ans = LinkedList()
     for x in tokens:
         temp = findRightWord(x)
+
